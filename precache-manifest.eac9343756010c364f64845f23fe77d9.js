31e9ff20abe2a5cb6a1168770738dd0b11aa28cc
Samuel Tracz <traczs@uoguelph.ca>
2020-08-24 22:30:11 -0400

    Updates

---
+++ precache-manifest.eac9343756010c364f64845f23fe77d9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "410b0639b0e44351de70341e008fd9b0",
+    "url": "/traczs.github.io/index.html"
+  },
+  {
+    "revision": "1f0f554dc92771a63e79",
+    "url": "/traczs.github.io/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "bc3bf583d937cd663959",
+    "url": "/traczs.github.io/static/css/main.0643ec6a.chunk.css"
+  },
+  {
+    "revision": "1f0f554dc92771a63e79",
+    "url": "/traczs.github.io/static/js/2.8a3750e3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/traczs.github.io/static/js/2.8a3750e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc3bf583d937cd663959",
+    "url": "/traczs.github.io/static/js/main.e851811f.chunk.js"
+  },
+  {
+    "revision": "7769c9ec806eaddd74ec",
+    "url": "/traczs.github.io/static/js/runtime-main.f6b83a56.js"
+  },
+  {
+    "revision": "4d918c26c2f69d79669c61ecde3c3eda",
+    "url": "/traczs.github.io/static/media/camis.4d918c26.jpeg"
+  },
+  {
+    "revision": "fdce6b02ddda1a16993c1a672392be60",
+    "url": "/traczs.github.io/static/media/cloud.fdce6b02.png"
+  },
+  {
+    "revision": "e8f1bcaf1a2aeb862f0d1e620a85d183",
+    "url": "/traczs.github.io/static/media/linkedin.e8f1bcaf.png"
+  },
+  {
+    "revision": "860864b6dbaa29e1474add765f8db8c3",
+    "url": "/traczs.github.io/static/media/rogers.860864b6.jpg"
+  },
+  {
+    "revision": "e38e0b49b43cc0b99e3e9fd452f638c0",
+    "url": "/traczs.github.io/static/media/tech.e38e0b49.jpeg"
+  }
+]);
